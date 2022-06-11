8c92c304e3b68bcfd2dcc7ed5d943214e5226bb7
Iman-Daneshi <iman_daneshi@yahoo.com>
2022-06-11 13:49:43 +0430

    changing admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'counted_view', 'status', 'published_date','created_date', 'updated_date')
+    list_filter  = ('status',)
+    ordering = ['-created_date',]
+    search_fields = ['title', 'content']
+
+admin.site.register(Post, PostAdmin)
 
