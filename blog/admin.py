6ade10e5fcbc94fd69dce47f658624e4165d4d11
Iman-Daneshi <iman_daneshi@yahoo.com>
2022-06-11 21:37:18 +0430

    add category field

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -10,5 +10,6 @@ class PostAdmin(admin.ModelAdmin):
     #ordering = ['-created_date',]
     search_fields = ['title', 'content']
 
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
 
--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tag
-    # category
+    category = models.ManyToManyField(Category,)
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False) 
     published_date = models.DateTimeField(null=True)
@@ -19,3 +26,4 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+        
