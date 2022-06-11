dfc4eb347d8ff02f4efd40f729428b7c13bdb0f8
Iman-Daneshi <iman_daneshi@yahoo.com>
2022-06-11 22:40:47 +0430

    add prev post and next post to single post

--- blog/views.py
+++ blog/views.py
@@ -10,5 +10,13 @@ def blog_view(request):
 def blog_single(request, pid):
     posts = Post.objects.filter(status=1)
     post = get_object_or_404(posts, pk=pid)
-    context = {'post': post}
+    index = list(posts).index(post)
+    next_post = posts[index-1] if index > 0 else None
+    prev_post = posts[index+1] if index < len(posts)-1 else None
+    context = {
+        'prev_post': prev_post,
+        'post': post,
+        'next_post': next_post,
+        
+        }
     return render(request, 'blog/blog-single.html', context)
