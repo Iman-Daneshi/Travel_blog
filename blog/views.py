12c6efa5a19af2e6da48becc10dda08fcf235a3d
Iman-Daneshi <iman_daneshi@yahoo.com>
2022-06-13 21:24:35 +0430

    bring posts base on author or category name

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
-def blog_view(request):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
     context = {'posts': posts} 
     return render(request, 'blog/blog-home.html', context)
 
@@ -22,8 +26,3 @@ def blog_single(request, pid):
     return render(request, 'blog/blog-single.html', context)
 
 
-def blog_category(request, cat_name):
-    posts = Post.objects.filter(status=1)
-    posts = posts.filter(category__name=cat_name)
-    context = {'posts': posts}
-    return render(request, 'blog/blog-home.html', context)
