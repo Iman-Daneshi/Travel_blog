03b2d263af17d1ddee1420d9912cf473d49c0883
Iman-Daneshi <iman_daneshi@yahoo.com>
2022-06-13 17:04:09 +0430

    add template tag to latest posts side bar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,13 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
+
+@register.simple_tag
+def function(a=7):
+    return a + 2
+
+@register.inclusion_tag('blog/blog-latest-posts.html')
+def latest_posts(arg=3):
+    latest_posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
+    return {'latest_posts': latest_posts}
