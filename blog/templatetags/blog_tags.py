5f43d568833edc795a5281eda87d8016533aa50d
Iman-Daneshi <iman_daneshi@yahoo.com>
2022-06-13 19:40:37 +0430

    add blog categories using template tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,13 +1,18 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
-
-@register.simple_tag
-def function(a=7):
-    return a + 2
 
 @register.inclusion_tag('blog/blog-latest-posts.html')
 def latest_posts(arg=3):
     latest_posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
     return {'latest_posts': latest_posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def post_categories ():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for cat_name in categories:
+        cat_dict[cat_name] = posts.filter(category=cat_name).count()
+    return {'cat_dict': cat_dict}
